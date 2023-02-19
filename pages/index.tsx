068205aadd721ec50604b632e1371d4ca8f06c4c
Oliver <wqdyxnbd@163.com>
2023-02-19 10:22:51 +0800

    add multiple lang

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,31 +6,38 @@ import { useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import DropDown, { VibeType } from "../components/DropDown";
 import Footer from "../components/Footer";
+// import Header from "../components/Header";
 import Github from "../components/GitHub";
-import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
 import ResizablePanel from "../components/ResizablePanel";
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState("");
-  const [vibe, setVibe] = useState<VibeType>("Professional");
-  const [generatedBios, setGeneratedBios] = useState<String>("");
+  const [desc, setDesc] = useState("");
+  const [lang, setLang] = useState<VibeType>("English");
+  const [generatedDescs, setGeneratedDescs] = useState<string>("");
+  const defaultDesc = 'How to write technological article.'
+  console.log("Streamed response: ", {generatedDescs});
+  let promptObj = {
+    'English': "UK English",
+    "中文": "Simplified Chinese",
+    "繁體中文": "Traditional Chinese",
+    "日本語": "Japanese",
+    "Italiano": "Italian",
+    "Deutsch": "German",
+    "Español": "Spanish",
+    "Français": "French",
+    "Nederlands": "Dutch",
+    "한국어": "Korean",
+    "ភាសាខ្មែរ":"Khmer",
+    "हिंदी" : "Hindi"
+  }
+  let text = desc||defaultDesc
+  const prompt = `Generate a answer in ${promptObj[lang]} that is friendly, but still professional and appropriate for the workplace The question topic is:${text}${text.slice(-1) === "." ? "" : "."}`
 
-  console.log("Streamed response: ", generatedBios);
-
-  const prompt =
-    vibe === "Funny"
-      ? `Generate 2 funny twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated bio is at max 20 words and base it on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
-        }`
-      : `Generate 2 ${vibe} twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure each generated bio is at least 14 words and at max 20 words and base them on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
-        }`;
-
-  const generateBio = async (e: any) => {
+  const generateDesc = async (e: any) => {
     e.preventDefault();
-    setGeneratedBios("");
+    setGeneratedDescs("");
     setLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -61,130 +68,121 @@ const Home: NextPage = () => {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
       const chunkValue = decoder.decode(value);
-      setGeneratedBios((prev) => prev + chunkValue);
+      setGeneratedDescs((prev) => prev + chunkValue);
     }
 
     setLoading(false);
   };
 
   return (
-    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
-      <Head>
-        <title>Twitter Generator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+        <Head>
+          <title>Answer Generator</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-          <p>Star on GitHub</p>
-        </a>
-        <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
-          Generate your next Twitter bio in seconds
-        </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
-        <div className="max-w-xl w-full">
-          <div className="flex mt-10 items-center space-x-3">
-            <Image
-              src="/1-black.png"
-              width={30}
-              height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
+        <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-2 sm:mt-4">
+          <div className="flex flex-wrap justify-center space-x-5">
+
+            <a
+                className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
+                href="https://github.com/Oliverwqcwrw/ai"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              <Github />
+              <p>Star on GitHub</p>
+            </a>
+          </div>
+
+          <h1 className="sm:text-3xl text-2xl max-w-1xl font-bold text-slate-900">
+            Generate your answer in seconds
+          </h1>
+          {/* <p className="text-slate-500 mt-5">18,167 bios generated so far.</p> */}
+          <div className="max-w-xl w-full">
+            <div className="flex mt-4 items-center space-x-3 mb-3">
+              <Image
+                  src="/1-black.png"
+                  width={30}
+                  height={30}
+                  alt="1 icon"
+              />
+              <p className="text-left font-medium">
+                Write a few sentences about your question.
+              </p>
+            </div>
+            <textarea
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+                rows={4}
+                className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-2"
+                placeholder={
+                    "e.g. "+defaultDesc
+                }
             />
-            <p className="text-left font-medium">
-              Copy your current bio{" "}
-              <span className="text-slate-500">
-                (or write a few sentences about yourself)
-              </span>
-              .
-            </p>
-          </div>
-          <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={
-              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
-            }
-          />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
+            <div className="flex mb-5 items-center space-x-3">
+              <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
+              <p className="text-left font-medium">Select your language.</p>
+            </div>
+            <div className="block">
+              <DropDown vibe={lang} setVibe={(newLang) => setLang(newLang)} />
+            </div>
 
-          {!loading && (
-            <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
-            >
-              Generate your bio &rarr;
-            </button>
-          )}
-          {loading && (
-            <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              disabled
-            >
-              <LoadingDots color="white" style="large" />
-            </button>
-          )}
-        </div>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-          toastOptions={{ duration: 2000 }}
-        />
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
-        <ResizablePanel>
-          <AnimatePresence mode="wait">
-            <motion.div className="space-y-10 my-10">
-              {generatedBios && (
-                <>
-                  <div>
-                    <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
-                      Your generated bios
-                    </h2>
-                  </div>
-                  <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                    {generatedBios
-                      .substring(generatedBios.indexOf("1") + 3)
-                      .split("2.")
-                      .map((generatedBio) => {
-                        return (
-                          <div
-                            className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+            {!loading && (
+                <button
+                    className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-4 mt-3 hover:bg-black/80 w-full"
+                    onClick={(e) => generateDesc(e)}
+                >
+                  Generate your Answer &rarr;
+                </button>
+            )}
+            {loading && (
+                <button
+                    className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-4 mt-3 hover:bg-black/80 w-full"
+                    disabled
+                >
+                  <LoadingDots color="white" style="large" />
+                </button>
+            )}
+          </div>
+          <Toaster
+              position="top-center"
+              reverseOrder={false}
+              toastOptions={{ duration: 2000 }}
+          />
+          <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+          <ResizablePanel>
+            <AnimatePresence mode="wait">
+              <motion.div className="space-y-10 my-4">
+                {generatedDescs && (
+                    <>
+                      <div>
+                        <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+                          Your generated Answer
+                        </h2>
+                      </div>
+                      <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto  whitespace-pre-wrap">
+
+                        <div
+                            className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border text-left"
                             onClick={() => {
-                              navigator.clipboard.writeText(generatedBio);
-                              toast("Bio copied to clipboard", {
+                              navigator.clipboard.writeText(generatedDescs);
+                              toast("Answer copied to clipboard", {
                                 icon: "✂️",
                               });
                             }}
-                            key={generatedBio}
-                          >
-                            <p>{generatedBio}</p>
-                          </div>
-                        );
-                      })}
-                  </div>
-                </>
-              )}
-            </motion.div>
-          </AnimatePresence>
-        </ResizablePanel>
-      </main>
-      <Footer />
-    </div>
+                        >
+                          <p>{generatedDescs}</p>
+                        </div>
+                      </div>
+                    </>
+                )}
+              </motion.div>
+            </AnimatePresence>
+          </ResizablePanel>
+        </main>
+        <Footer />
+      </div>
   );
 };
 
